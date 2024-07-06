f0632545c644217b8324484732ecbb7ad6f27000
Hopppangman <ghdudrur@gmail.com>
2024-07-06 17:12:33 +0900

    field search 추가, js 추가 변경

--- src/main/resources/js/issue-history-tracker.js
+++ src/main/resources/js/issue-history-tracker.js
@@ -11,37 +11,6 @@ WRMRequire('wr!com.atlassian.jira.jira-frontend-plugin:entrypoint-activityTabs')
     });
 });
 
-AJS.tablessortable.setTableSortable(AJS.$(".aui-table-sortable"));
-AJS.$("#issue-tab-panel-user-selector").auiSelect2();
-AJS.$('#issue-tab-panel-user-selector').on('change',function(e){
-    let selectedUser = e.target.value;
-    for( let user of AJS.$('#issue-tab-panel-result-table > tbody > tr > td[headers="updated-user"]')) {
-        let trUser = AJS.$(user);
-        if(selectedUser == user.getAttribute('value')){
-            trUser.parent().show();
-        }
-        else{
-            trUser.parent().hide();
-        }
-    }
-});
-
-AJS.$('#issue-tab-panel-field-selector').on('change',function(e){
-    let selectedField = e.target.value;
-    for( let field of AJS.$('#issue-tab-panel-result-table > tbody > tr > td[headers="updated-field"]')) {
-        let trField = AJS.$(field);
-        if(selectedField.toLowerCase() == field.getAttribute('value').toLowerCase()){
-            trField.parent().show();
-            console.log("Show search field : ", selectedField);
-            console.log("Show field : ", field.getAttribute('value'));
-        }
-        else{
-            trField.parent().hide();
-            console.log("Hide search field : ", selectedField);
-            console.log("Hide field : ", field.getAttribute('value'));
-        }
-    }
-});
 
 JIRA.ViewIssueTabs.onTabReady(function() {
     AJS.$('.tab-content').find('#custom-issue-tabpanel').addClass('hidden');
